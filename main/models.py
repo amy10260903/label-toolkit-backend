e9a399c0bcbd78c97361ba284573361a7e9524bb
amy10260903 <amy10260903@gmail.com>
2021-01-08 16:03:15 +0800

    [feat] test

--- main/models.py
+++ main/models.py
@@ -25,27 +25,3 @@ class Fingerprint(models.Model):
 
     class Meta:
         unique_together = ('recording', 'hash', 'offset')
-
-# class RecordingWithNR(models.Model):
-#     id = models.AutoField(primary_key=True, unique=True)
-#     filename = models.CharField(null=False, max_length=250, help_text='錄音檔名')
-#     fingerprinted = models.BooleanField(null=True, default=False, help_text='指紋識別狀態')
-#     file_sha1 = models.CharField(max_length=160, null=False, help_text='')
-#     total_hashes = models.IntegerField(null=False, default=0, help_text='指紋總數')
-#     date_created = models.DateTimeField(auto_now_add=True, help_text='建立時間')
-#     date_modified = models.DateTimeField(auto_now=True, help_text='更新時間')
-#
-#     def __str__(self):
-#         return self.filename
-#
-# class FingerprintWithNR(models.Model):
-#     id = models.AutoField(primary_key=True, unique=True)
-#     recording = models.ForeignKey(RecordingWithNR, null=False, on_delete=models.CASCADE, help_text='音檔id')
-#     hash = models.CharField(max_length=80, null=False, help_text='指紋加密')
-#     offset = models.IntegerField(null=False, help_text='指紋起始點')
-#     date_created = models.DateTimeField(auto_now_add=True, help_text='建立時間')
-#     date_modified = models.DateTimeField(auto_now=True, help_text='更新時間')
-#
-#     class Meta:
-#         unique_together = ('recording', 'hash', 'offset')
-
