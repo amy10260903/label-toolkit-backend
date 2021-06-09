8e379aea4642bc3d7a02c6029c3434c930c37aff
amy10260903 <amy10260903@gmail.com>
2021-06-10 03:05:03 +0800

    [feat] alert for selecting nothing

--- static/js/demo.js
+++ static/js/demo.js
@@ -117,6 +117,10 @@ Loader.hide = function() {
  */
 function startAnalysis() {
     console.log('startAnalysis');
+    if ($(".current")[0].innerHTML=="Source" || $(".current")[1].innerHTML=="Sound event") {
+        alert('Please select a dataset and an event!');
+        return;
+    }
     const data = {
         category: $(".current")[0].innerHTML,
         // file: $("#upload-btn").prop('files')[0],
