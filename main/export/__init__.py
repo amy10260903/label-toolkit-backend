985a6cf78e026e561caa05e47bf94bcdefee751b
amy10260903 <amy10260903@gmail.com>
2021-06-21 01:14:24 +0800

    [feat] exporting userlog

--- main/export/__init__.py
+++ main/export/__init__.py
@@ -1,34 +1,44 @@
 from openpyxl import Workbook
-import requests
 import os
 import time
 
-from main.export.config import grouplist, mappinglist
 from main.export.api import Userlog
+from main.export.config import \
+    exportlist, grouplist, mappinglist
 
 headers = ['Filename', 'Labeling Time (mm:ss)']
 base_dir = os.path.dirname(os.getcwd())
-result_dir = os.path.join(base_dir, 'Research', 'LabelToolkit', 'usertest', 'result')
+result_dir = os.path.join(base_dir, 'Research', 'LabelToolkit', 'usertest', 'result', 'user')
 
-def group():
+def exportUserRecord():
     api = Userlog()
     for group, users in grouplist.items():
         for username, request in users.items():
-            dirname = os.path.join(result_dir, group, username, mappinglist[group])
-            # print(dirname)
-            if username == 'Jim':
+            if username not in exportlist:
+                dirname = os.path.join(result_dir, group, username, mappinglist[group])
+
                 data = []
                 for rid in request:
-                    response = api.get(rid-4)
+                    response = api.get(rid)
                     data.extend(response)
-                print(data)
+
                 exportToExcel(dirname, 'record', data)
+                print(f"export to {username} / {os.path.basename(dirname)}")
+                exportlist.append(username)
+    print(f"Exported: {exportlist}")
 
 def exportToExcel(dirname, filename, data):
     wb = Workbook()
     page = wb.active
     page.append(headers)
     for row in data:
+        if row['id'] == 5:
+            row['labeled_time'] = row['labeled_time'] - 800
+        if row['id'] in [51, 88, 99, 143]:
+            continue
+        if row['id'] in [11, 87, 117, 150]:
+            row['filename'] = 'audio_08'
+            # continue
         labeled_time = time.strftime('%M:%S', time.gmtime(row['labeled_time']))
         page.append([row['filename'], labeled_time])
 
--- main/export/config.py
+++ main/export/config.py
@@ -1,17 +1,28 @@
-BaseURL = "http://localhost:8000/"
+BaseURL = "http://ahg.ee.nthu.edu.tw/"
+# BaseURL = "http://localhost:8000/"
 
 mappinglist = {
     'Group1': 'Task2',
     'Group2': 'Task1',
 }
 grouplist = {
-    'Group1' : {
+    'Group1': {
         'Jim': [23, 24],
         'Grace': [26, 27],
         'Vodka': [29, 30],
+        'Tomic': [48, 49],
+        'Charles': [52, 53],
+        'Chu': [60, 61]
     },
-    'Group2' : {
+    'Group2': {
         'Andy': [13, 14],
-        'Adam': [35, 36]
+        'Adam': [35, 36],
+        'Jenna': [46, 47],
+        'William': [44, 45],
+        'Neil': [64, 65],
     }
 }
+# exportlist = []
+exportlist = ['Jim', 'Grace', 'Vodka', 'Andy', 'Adam',
+              'Tomic', 'Jenna', 'William', 'Charles', 'Chu',
+              'Neil']
